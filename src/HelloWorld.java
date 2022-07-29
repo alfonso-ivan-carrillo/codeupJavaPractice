9b022227634dddf88f1b225afbe539261fb1177f
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-07-29 11:32:39 -0500

    lesson  one

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("Hello World!");
+
+        System.out.println("Code inside of curly braces should be indented");
+        System.out.println("with four spaces");
+        System.out.println("\""); // => "
+        System.out.println("\\"); // => \
+        System.out.println("new line \n"); // => the newline character
+        System.out.println("tab \t"); // => the tab character
     }
 }
