2404b0b78a1a690622fac93c4efc1bea2356cf5d
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-03 07:49:21 -0500

    codeup control statement lesson

---
+++ src/ControlStatemetnsAndLoopsLesson.java
@@ -0,0 +1,12 @@
+public class ControlStatemetnsAndLoopsLesson {
+
+    public static void main(String[] args) {
+
+        String inputString = "y";
+        String userInput = "y";
+
+        System.out.println(inputString == userInput);
+        System.out.println(inputString.equals(userInput));
+
+    }
+}
