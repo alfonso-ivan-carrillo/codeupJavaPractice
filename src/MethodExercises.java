b0088fe3d2ac9b518a5d872ca89b91058b6792f1
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-14 15:05:25 -0500

    Codeup Methods Execrises Part 1 Bonus

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -25,6 +25,34 @@ public class MethodExercises {
         return num1 % num2;
     }
 
+    // Bonus
+
+    public static int multLoop(int num1, int num2){
+
+        int total = 0;
+        for(int i = 1; i <= num2; i++){
+         total += num1;
+//            System.out.println(total);
+        }
+        return total;
+    }
+
+    public static int multRec(int num1, int num2){
+        int total = 0;
+        if (num2 == 0){
+            return 0;
+        } else {
+//            System.out.println(num1);
+//            System.out.println(num2 - 1);
+
+            total += num1 + multRec(num1, num2 - 1);
+//            System.out.println(num1);
+//            System.out.println(num2 - 1);
+//            System.out.println("total: "+ total);
+            return total;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println(add(5, 10));
@@ -33,6 +61,9 @@ public class MethodExercises {
         System.out.println(div(10, 5));
         System.out.println(mod(5,2));
         System.out.println(div(5,0));
+
+        System.out.println(multLoop(5,5));
+        System.out.println(multRec(5,5));
 
 
     }
