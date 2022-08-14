0a72ea2bc67982cfd540dbdcc0d777c843bc822c
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-14 00:15:54 -0500

    Codeup Methods Lesson

--- src/MethodsLesson.java
+++ src/MethodsLesson.java
@@ -26,7 +26,41 @@ public class MethodsLesson {
         return s.toUpperCase() + "!!!";
     }
 
+    public static void changeString(String s) {
+        s = "Wub a lub a dub dub";
+    }
+
+    public static void count (int n){
+        if(n <= 0){
+            System.out.println("All done!");
+            return;
+        }
+        System.out.println(n);
+        count(n - 1);        // calls itself but reduce the value by 1 until n == 0
+    }
+
+//    public static long getPower(int base, int exponent) {
+//        long result = 1;
+//        for (int i = 1; i <= exponent; i++) {
+//            result = result * base;
+//        }
+//        return result;
+//    }
+
+    public static long getPower(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        } else if (exponent == 2) {
+            return base * base;
+        }
+        return base * getPower(base, exponent - 1);
+    }
     public static void main(String[] args) {
+
+        System.out.println(getPower(2, 3));
+
 
         sayHello(5);
 
@@ -38,8 +72,13 @@ public class MethodsLesson {
 
         System.out.println(shout("party"));
 
+        String codeUp = "Codeup";
 
+        changeString(codeUp);
 
+        System.out.println(codeUp); // Value still Codeup
+
+        count(5);
 
     }
 }
