5d579e7b7ee5e0592677bce014f261f449dcebf4
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-15 21:46:31 -0500

    Codeup Methods Exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -8,19 +8,24 @@ public class HighLow {
         Scanner scan = new Scanner(System.in);
         System.out.println("Guess a number between 1 and 100");
         int userGuess = scan.nextInt();
+        String keepGoing = "yes";
 
 
-        while (userGuess != cpuNumber) {
+        while(keepGoing == "yes") {
             if (userGuess < 0 || userGuess > 100) {
                 System.out.println("Number out of range.");
                 highLow();
             } else if (userGuess < cpuNumber) {
                 System.out.println("Higher!");
+                System.out.println("Guess again.");
+                userGuess = scan.nextInt();
             } else if (userGuess > cpuNumber) {
                 System.out.println("Lower!");
+                System.out.println("Guess again.");
+                userGuess = scan.nextInt();
             } else if (userGuess == cpuNumber) {
                 System.out.println("Good Guess!");
-                userGuess = cpuNumber;
+                keepGoing = "no";
             } else {
                 System.out.println("error");
             }
