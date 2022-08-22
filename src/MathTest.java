da7fffb8aa67725c2475ce80ce3aae262a1ad0e8
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-22 10:46:34 -0500

    Codeup OOP Visibility Lesson

--- src/MathTest.java
+++ src/MathTest.java
@@ -5,5 +5,15 @@ public class MathTest {
         System.out.println(Arithmetic.add(5, 5));
         System.out.println(Arithmetic.multiply(5, 5));
         System.out.println(Arithmetic.pi);
+
+        System.out.println("-------------");
+
+//      from student class
+        Student s3 = new Student("Student C", 95.5);
+        System.out.println(s3.name);  // prints "Student C"
+//      System.out.println(s3.grade);  // DON'T DO THIS - private properties can't be accessed outside the class
+                                        // will result in compile time error
+        System.out.println(s3.shareGrade());  // do this instead
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -2,12 +2,18 @@ public class Student {
     public String name;
     public String cohort;
 
+    private double grade;
+
     public Student(String studentName) {   // Constructor
 //        name = studentName;
 //        cohort = "Unassigned";
         this(studentName, "Unassigned");
     }
 
+    public Student (String studentName, double grade){
+        this.name = studentName;
+        this.grade = grade;
+    }
 
     public Student(String studentName, String assignedCohort) { // Constructor
 //        name = studentName;
@@ -25,12 +31,33 @@ public class Student {
         return String.format("name: %s, cohort: %s", name, cohort);
     }
 
+    public double shareGrade(){
+        return grade;
+    }
+    // Visibility refers to how a class or instance field is allowed to be accessed. It allows us to encapsulate
+    // the data in our classes; that is, only expose what is necessary to other classes and objects.
+
+    // Thus far, we have used the public keyword for all of our fields. The public keyword means that the field is
+    // accessible from everywhere. The next visibility keyword we'll introduce is private. When a field is
+    // defined as private, it is only accessible within the class that defines it.
+
+    // ***  A common pattern in Java is to define classes with private fields, and create methods to allow
+    // access to those fields. ***
+
 
     public static void main(String[] args) {
         Student s1 = new Student("Student A");
         Student s2 = new Student("Student B", "Voyagers");
         System.out.println(s1.getInfo()); // "name: Student A cohort: Unassigned"
         System.out.println(s2.getInfo()); // "name: Student B cohort: Voyagers"
-    }
 
+        System.out.println("------------");
+
+        Student s4 = new Student("joe", 80.0);
+        System.out.println(s4.name);
+        System.out.println(s4.grade);  // the private field will print out since its within the same class
+        System.out.println(s4.getInfo());  // if trying to access  field out of class, would use the method created to
+        // access private field
+
+    }
 }
