89873a8feabfb321d3f384399798c0fd127086f4
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-24 14:43:52 -0500

    Codeup OOP Exercises Question 3

---
+++ src/Shapes/CircleApp.java
@@ -0,0 +1,14 @@
+package Shapes;
+
+public class CircleApp {
+
+
+    public static void main(String[] args) {
+
+
+        Circle one = new Circle();
+        System.out.println(one.getCir(5));
+        System.out.println(one.gitArea(5));
+
+    }
+}
