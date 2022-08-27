b475475687b24c78e8abf7a166cd40321f4cdd43
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-27 00:53:12 -0500

    Codeup Arrays Lesson

---
+++ src/ArrayLesson.java
@@ -0,0 +1,13 @@
+public class ArrayLesson {
+
+    public static double[] prices;
+
+
+
+
+    public static void main(String[] args) {
+
+        prices = new double[4];
+        System.out.println(prices);
+    }
+}
