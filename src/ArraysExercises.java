63ed33d46bb07c67e1b9bc61496f0f98f3e08893
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-08-29 21:15:50 -0500

    Codeup Array Exercises Question 1 completed

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,9 +5,26 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-//    public static Object addPerson(Array list, Object Person){
-//        Person[] newList = list;
-//    }
+    public static Person[] addPerson(Person persons[], Person person){
+            //   Array type          Object Array ,    Object Person
+        System.out.println(persons.length);  // check lenght of array
+
+        // new array or copy of array created / referenced by same name, if you
+        // try to create a new array, will give you an error because new array element will
+        // will return null, so pass in the same name array since it s a copy
+        persons = Arrays.copyOf(persons, persons.length + 1);
+        // Array type Person - name of new array = Arrays Method ( array, array.length +1 is the new length) making room for new object
+        persons[persons.length -1] = person;
+        // new Array [array.length - 1 (last spot of array)] = object passed into method
+        System.out.println(persons.length);  // check length - is it longer than passed array
+
+        System.out.println(persons.length);
+        for(int i = 0; i < persons.length; i++){
+            System.out.println(persons[i].getName());
+        }
+        System.out.println(persons.length);
+        return persons;
+    }
 
     public static void main(String[] args) {
 
@@ -21,8 +38,13 @@ public class ArraysExercises {
         personList[2] = new Person("alfonso");
 
         for(int i = 0; i < personList.length; i++){
-            System.out.println(personList[i].name);
+            System.out.println(personList[i].getName());
         }
+        Person bella = new Person("bella");
+
+
+
+        addPerson(personList, bella );
 
     }
 }
