276f67dd8d8c83cb03249d026a6df03796be0991
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-09-05 16:13:48 -0500

    Codeup adding to heromaker class

--- src/marvel/HeroMaker.java
+++ src/marvel/HeroMaker.java
@@ -9,14 +9,34 @@ public class HeroMaker {
 //    HeroMaker makers;
     public static void maker(){
         Input one = new Input();
+        String answer = one.getString("Do you want to create a Hero or a Villian?");
+        if(answer.equalsIgnoreCase("hero")) {
 
-        do {
-            Hero hero = new Hero();
-            hero.setName(one.getString("Enter hero's name:"));
-            hero.setPower(one.getString("Power? (mutant, artificial or other)"));
-            System.out.printf("Hero's name: %s and hero's power: %s. \n", hero.getName(), hero.getPower());
-            System.out.println(hero.team());
-        } while (one.yesNo("Make another hero?"));
+            do {
+                Hero hero = new Hero();
+                hero.setName(one.getString("Enter hero's name:"));
+                hero.setPower(one.getString("Power? (mutant, artificial or other)"));
+                hero.setAlterEgo(one.getString("Do they have an alterego?"));
+                System.out.printf("Hero's name: %s, hero's power: %s, hero's alterego: %s. \n", hero.getName(), hero.getPower(), hero.getAlterEgo());
+                System.out.println(hero.team());
+            } while (one.yesNo("Make another hero?"));
+            maker();
+        } else if (answer.equalsIgnoreCase("villian")){
+            do{
+                Villian villian = new Villian();
+                villian.setName(one.getString("Enter Villian's name:"));
+                villian.setPower(one.getString("Power? (mutant, artificial or other"));
+                villian.setAlterEgo(one.getString("Enter alterego: "));
+                System.out.printf("Villian's name: %s, power: %s and alterego: %s.\n", villian.getName(), villian.getPower(), villian.getAlterEgo() );
+                System.out.println(villian.team());
+            } while (one.yesNo("Make another villian?"));
+            maker();
+        } else if (answer.equalsIgnoreCase("no")){
+            System.out.println("Exiting");
+        } else {
+            System.out.println("Not a hero or a villian");
+            maker();
+        }
     }
 
 //    public static void heroList(){
@@ -71,7 +91,8 @@ public class HeroMaker {
         System.out.println(wolverine.team());
 
         System.out.println("----------------");
-        System.out.println("----------------");
+
+        maker();
 
 
 
