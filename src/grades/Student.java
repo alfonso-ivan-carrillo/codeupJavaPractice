1bd7b293bfef870d8b2e753b0ca586e422f1444a
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-09-09 12:45:24 -0500

    Codeup Collections Exercise Question 1

---
+++ src/grades/Student.java
@@ -0,0 +1,48 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(){
+    }
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+//    public double getGradeAverage(){
+//        double sum = 0;
+//        for(int i = 0; i < grades.size(); i++){
+//            sum += grades.get(i);   // grades.get(i) get the actual grade, if you only use i, then it adds the index of element
+////            System.out.println(grades.get(i));
+//        }
+//        return (sum / grades.size());
+//    }
+
+    public double getGradeAverage(){
+        double sum = 0;
+        for ( int grade : grades){  // takes the value of the element instead of the index
+            sum += grade;
+        }
+        return (sum / grades.size());
+    }
+
+
+}
---
+++ src/grades/StudentTest.java
@@ -0,0 +1,19 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+
+        Student alfonso = new Student("alfonso"  );
+        alfonso.addGrade(100);
+        alfonso.addGrade(80);
+        alfonso.addGrade(90);
+        System.out.println(alfonso.getName());
+        System.out.println("The average grade for this student is: " + alfonso.getGradeAverage());
+        System.out.println(alfonso.getGrades());
+
+    }
+}
