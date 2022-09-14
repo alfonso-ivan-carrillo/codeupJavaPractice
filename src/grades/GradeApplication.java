17ec3d3f95fb3f8c873514806effbdd621c1264e
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-09-14 12:30:02 -0500

    Codeup Collections Exercises Question 3 completed

--- src/grades/GradeApplication.java
+++ src/grades/GradeApplication.java
@@ -1,33 +1,59 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradeApplication {
+    private static Input input = new Input();
 
 
     public static void gradeReport(){
         HashMap<String, Student> students = new HashMap<>();
-        Student alfonso = new Student("alfonso");
+        Student alfonso = new Student("alfonso carrillo");
         alfonso.addGrade(90);
         alfonso.addGrade(100);
         alfonso.addGrade(80);
 
-        Student kathy = new Student("kathy");
+        Student kathy = new Student("kathy maddocks");
         kathy.addGrade(99);
         kathy.addGrade(95);
         kathy.addGrade(90);
 
-        Student ethan = new Student("ethan");
+        Student ethan = new Student("ethan carrillo");
         ethan.addGrade(90);
         ethan.addGrade(95);
         ethan.addGrade(100);
 
-        students.put("alfonso", alfonso);
-        students.put("kathy", kathy);
-        students.put("ethan", ethan);
-        System.out.println("Welcome!");
-        System.out.println("Here are the GitHub usernames of our students:\n");
-        System.out.println(students.keySet());
+        students.put("mekca", alfonso);
+        students.put("bellaGit", kathy);
+        students.put("busterGit", ethan);
+
+
+        System.out.println("Welcome!\n");
+
+        do{
+            System.out.println("Here are the GitHub usernames of our students:\n");
+            System.out.println(students.keySet());
+            System.out.println("\nWhat student would you like to see more information on?");
+            Scanner scn = new Scanner(System.in);
+            String userInput = scn.nextLine();
+            System.out.println(students.containsKey(userInput));
+            for(int i = 0; i < students.size(); i++){
+                if(students.containsKey(userInput)){
+                    System.out.printf("name: %s -- GitHub Usernames: %s \nGrade Average: %.2f \n", students.get(userInput).getName(), userInput, students.get(userInput).getGradeAverage());
+                    break;
+                } else {
+                    System.out.printf("Sorry, no student found with the GitHub username of | %s | \n", userInput);
+                    break;
+                }
+            }
+        }while (input.yesNo("\nDo you want to continue?"));
+
+
+
+
 
     }
 
@@ -49,11 +75,14 @@ public class GradeApplication {
 //        ethan.addGrade(95);
 //        ethan.addGrade(100);
 //
-//        students.put("alfonso", alfonso);
+//        students.put("mekca", alfonso);
 //        students.put("kathy", kathy);
 //        students.put("ethan", ethan);
 //
 //        System.out.println(students.keySet());
+//        System.out.println(students.containsKey("mekca"));
+
+
 
 
         gradeReport();
